9b2caae89e4bda7e1fde04a40c0cc090e410ce6a
Matthew C <matthew@mcharlsto.uk>
2022-11-07 23:21:03 +0000

    Update paths

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cp /root/firstboot/os-release /etc/os-release # set os details
-cp /root/firstboot/os-release /etc/initrd-release
-cp /root/firstboot/os-release /usr/lib/os-release
+cp /root/femarch-bootstrap/os-release /etc/os-release # set os details
+cp /root/femarch-bootstrap/os-release /etc/initrd-release
+cp /root/femarch-bootstrap/os-release /usr/lib/os-release
 
-cp /root/firstboot/sudoers /etc/ # ferdi's stuff
-cp /root/firstboot/pacman.conf /etc
+cp /root/femarch-bootstrap/sudoers /etc/ # ferdi's stuff
+cp /root/femarch-bootstrap/pacman.conf /etc
 
-cd /root/firstboot # install uwufetch
+cd /root/femarch-bootstrap # install uwufetch
 git clone https://github.com/femarch/based-uwufetch
 cd based-uwufetch
 make
@@ -19,4 +19,5 @@ cp -r res/* /usr/lib/uwufetch
 cp default.config /etc/uwufetch/config
 cp ./uwufetch.1.gz /usr/share/man/man1
 
-rm -rf /root/firstboot
+touch /bootstrap.ran
+rm -rf /root/femarch-bootstrap
