636bf411826617eca85da502fd3acb8b7347cf8e
Matthew C <matthew@mcharlsto.uk>
2022-11-08 15:48:34 +0000

    Femboiz

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+systemctl start NetworkManager
 cp /root/femarch-bootstrap/os-release /etc/os-release # set os details
 cp /root/femarch-bootstrap/os-release /etc/initrd-release
 cp /root/femarch-bootstrap/os-release /usr/lib/os-release
@@ -23,7 +24,7 @@ pacman-key --init
 pacman-key --populate nicroxio
 pacman-key --lsign-key nicroxio
 
-pacman -Sy calamares
+pacman --no-confirm -Sy calamares
 
 touch /bootstrap.ran
 rm -rf /root/femarch-bootstrap
