0a89b87003a496f46bab343e733bd9b5174b7835
Matthew C <matthew@mcharlsto.uk>
2022-11-10 14:55:51 +0000

    Update script

--- install.sh
+++ install.sh
@@ -10,25 +10,27 @@ cp /root/femarch-bootstrap/os-release /usr/lib/os-release
 cp /root/femarch-bootstrap/sudoers /etc/ # ferdi's stuff
 cp /root/femarch-bootstrap/pacman.conf /etc
 
-cd /root/femarch-bootstrap # install uwufetch
-git clone https://github.com/femarch/based-uwufetch
-cd based-uwufetch
-make
-mkdir -pv /usr/bin /usr/lib/uwufetch /usr/share/man/man1 /etc/uwufetch
-cp uwufetch /usr/bin
-cp libfetch.so /usr/lib
-cp fetch.h /usr/include
-cp -r res/* /usr/lib/uwufetch
-cp default.config /etc/uwufetch/config
-cp ./uwufetch.1.gz /usr/share/man/man1
+#cd /root/femarch-bootstrap # install uwufetch
+#git clone https://github.com/femarch/based-uwufetch
+#cd based-uwufetch
+#make
+#mkdir -pv /usr/bin /usr/lib/uwufetch /usr/share/man/man1 /etc/uwufetch
+#cp uwufetch /usr/bin
+#cp libfetch.so /usr/lib
+#cp fetch.h /usr/include
+#cp -r res/* /usr/lib/uwufetch
+#cp default.config /etc/uwufetch/config
+#cp ./uwufetch.1.gz /usr/share/man/man1
 
 pacman-key --init
+sleep 1
 gpg --import /usr/share/pacman/keyrings/femarch.gpg
 pacman-key --add /usr/share/pacman/keyrings/femarch.gpg
 pacman-key --lsign-key 7062A8668D1D930BF985FD5DB52216032B237E33
-
+sleep 1
 
 pacman -Sy calamares
 
 touch /bootstrap.ran
+echo Bootstrapped!
 rm -rf /root/femarch-bootstrap
